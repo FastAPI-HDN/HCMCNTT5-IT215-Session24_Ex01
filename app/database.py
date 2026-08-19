46bd188b451106f662290c245fa10c9f4f56ae2d
Hoang Duong Nam <hoangduongnampb.2k6@gmail.com>
2026-08-19 08:05:44 +0700

    bai01

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 
-DATABASE_URL = "mysql+pymysql://root:01884814110Loc*@localhost:3306/SS24_IT215_BTTH1"
+DATABASE_URL = "mysql+pymysql://root:Dnam14012006!@localhost:3306/SS24_IT215_BTTH1"
 
 Engine = create_engine(DATABASE_URL)
 
